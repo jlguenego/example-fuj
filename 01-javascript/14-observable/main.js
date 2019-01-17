2a0626a37f2e2039886c51b62aeec178c67626ea
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-01-17 17:56:57 +0100

    obs

--- 01-javascript/14-observable/main.js
+++ 01-javascript/14-observable/main.js
@@ -5,4 +5,7 @@ interval(200).pipe(
   take(11),
   filter(x => x % 2 === 0),
   map(x => x + x)
-).subscribe(x => console.log(x));
+).subscribe({
+  next: x => console.log(x),
+  complete: x => console.log('complete', x)
+});
