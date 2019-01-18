d9a684f7a9fb70c2a4809ea8ef038da6fb0e2faf
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-01-18 16:13:22 +0100

    express

--- 02-node/03-express/server.ts
+++ 02-node/03-express/server.ts
@@ -3,13 +3,14 @@ import * as serveIndex from 'serve-index';
 import * as fs from 'fs';
 
 const port = 9000;
+const context = { time: () => new Date() };
 
 const app = express();
 app.set('views', './tmpl');
 app.set('view engine', 'ejs');
 app.get('**', function (req, res, next) {
     if (fs.existsSync('./tmpl' + req.url + '.ejs')) {
-        res.render('clock', { time: () => new Date() });
+        res.render('.' + req.url, context);
         return;
     }
     next();
