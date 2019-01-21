157fcf045ee5203b760096dd7777c7ec77511a10
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-01-21 14:01:07 +0100

    responsive

--- 03-angular/02-tickets/src/app/layout/header/header.component.ts
+++ 03-angular/02-tickets/src/app/layout/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +10,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  isHandsetPortrait$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.HandsetPortrait)
+    .pipe(
+      map(result => result.matches)
+    );
 
-  ngOnInit() {
-  }
+  constructor(private breakpointObserver: BreakpointObserver) { }
+
+  ngOnInit() { }
 
 }
