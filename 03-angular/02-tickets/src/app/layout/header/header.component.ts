e579b52da0c100c11e6ea2cbcb6a2639d963f427
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-01-21 14:54:30 +0100

    mobile menu working

--- 03-angular/02-tickets/src/app/layout/header/header.component.ts
+++ 03-angular/02-tickets/src/app/layout/header/header.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
 import { MenuService } from '../menu.service';
 
 @Component({
@@ -11,12 +8,7 @@ import { MenuService } from '../menu.service';
 })
 export class HeaderComponent implements OnInit {
 
-  isHandsetPortrait$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.HandsetPortrait)
-    .pipe(
-      map(result => result.matches)
-    );
-
-  constructor(private breakpointObserver: BreakpointObserver, public menu: MenuService) { }
+  constructor(public menu: MenuService) { }
 
   ngOnInit() { }
 
--- 03-angular/02-tickets/src/app/layout/menu.service.ts
+++ 03-angular/02-tickets/src/app/layout/menu.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +11,19 @@ export class MenuService {
   isOpen = false;
   state$ = new BehaviorSubject<boolean>(false);
 
-  constructor() { }
+  isHandsetPortrait$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.HandsetPortrait)
+    .pipe(
+      map(result => result.matches),
+      map(isHandset => {
+        if (isHandset === false) {
+          this.isOpen = false;
+          this.state$.next(this.isOpen);
+        }
+        return isHandset;
+      }),
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver) { }
 
   toggle() {
     console.log('toggle');
