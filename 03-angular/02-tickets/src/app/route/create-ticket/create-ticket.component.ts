a2fcea4265383d179b4359318e3f0bc2c2e20c5a
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-01-22 10:56:34 +0100

    form

--- 03-angular/02-tickets/src/app/route/create-ticket/create-ticket.component.ts
+++ 03-angular/02-tickets/src/app/route/create-ticket/create-ticket.component.ts
@@ -7,12 +7,13 @@ import { FormBuilder, Validators } from '@angular/forms';
   styleUrls: ['./create-ticket.component.scss']
 })
 export class CreateTicketComponent implements OnInit {
-  
+
   ticketForm = this.fb.group({
     title: [null, Validators.required],
-    description: [null, Validators.required],
+    description: [null, Validators.compose([Validators.required, Validators.minLength(20)])],
     firstName: [null, Validators.required],
     lastName: [null, Validators.required],
+    email: [null, Validators.compose([Validators.required, Validators.email])],
     project: [null, Validators.required],
     isUrgent: [false, Validators.required],
     // postalCode: [null, Validators.compose([
@@ -24,19 +25,23 @@ export class CreateTicketComponent implements OnInit {
   hasUnitNumber = false;
 
   projects = [
-    {label: 'IHM IR', value: 'IHM_IR'},
-    {label: 'DB IR', value: 'DB_IR'},
-    {label: 'Migration IR', value: 'MIGRATION_IR'},
-    {label: 'IHM CRM', value: 'IHM_CRM'},
+    { label: 'IHM IR', value: 'IHM_IR' },
+    { label: 'DB IR', value: 'DB_IR' },
+    { label: 'Migration IR', value: 'MIGRATION_IR' },
+    { label: 'IHM CRM', value: 'IHM_CRM' },
   ];
 
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
   }
 
   onSubmit() {
     alert('Thanks!');
+  }
+
+  autoGenerate() {
+    alert('generate');
   }
 
 }
