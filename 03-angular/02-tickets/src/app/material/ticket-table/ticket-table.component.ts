4cd3441aaea55a2b46aee7472ccc813ff6432945
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-01-22 17:33:08 +0100

    highlight

--- 03-angular/02-tickets/src/app/material/ticket-table/ticket-table.component.ts
+++ 03-angular/02-tickets/src/app/material/ticket-table/ticket-table.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { MatPaginator, MatSort } from '@angular/material';
+import { MatPaginator, MatSort, MatCheckboxChange } from '@angular/material';
 import { TicketTableDataSource } from './ticket-table-datasource';
 import { TicketsService } from 'src/app/tickets.service';
 import { TicketRecord } from 'src/app/ticket.record';
@@ -16,8 +16,9 @@ export class TicketTableComponent implements OnInit {
 
   /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
   displayedColumns = ['check', 'dueDate', 'title', 'firstName', 'lastName'];
+  selectedRowIndexes = new Set<number>();
 
-  constructor(private tickets: TicketsService) {}
+  constructor(private tickets: TicketsService) { }
 
   ngOnInit() {
     this.dataSource = new TicketTableDataSource(this.paginator, this.sort, this.tickets);
@@ -25,5 +26,22 @@ export class TicketTableComponent implements OnInit {
 
   viewTicket(row: TicketRecord) {
     this.tickets.view(row.id);
+  }
+
+  onCheckBoxChange(event: MatCheckboxChange, row) {
+    console.log('event', event);
+    if (event.checked) {
+      this.highlight(row);
+    } else {
+      this.unHighlight(row);
+    }
+  }
+
+  highlight(row: TicketRecord) {
+    this.selectedRowIndexes.add(row.id);
+  }
+
+  unHighlight(row: TicketRecord) {
+    this.selectedRowIndexes.delete(row.id);
   }
 }
