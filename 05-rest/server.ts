3ef2b5a62c36c7f97010a5a0843af816e29b2649
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-01-27 19:02:45 +0100

    docker

--- 05-rest/server.ts
+++ 05-rest/server.ts
@@ -6,7 +6,7 @@ import { ws } from './ws';
 const port = 9000;
 
 const app = express();
-app.use('/ws', ws);
+// app.use('/ws', ws);
 app.use(express.static('www'));
 app.use(serveIndex('www', { icons: true }));
 app.listen(port, () => console.log('Server started on port', port));
