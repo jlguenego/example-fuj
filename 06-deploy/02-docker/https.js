40688347718b8f2bc341728960a212428bb67432
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-01-29 11:36:24 +0100

    https

--- 06-deploy/02-docker/https.js
+++ 06-deploy/02-docker/https.js
@@ -3,6 +3,6 @@ const fs = require('fs');
 const path = require('path');
 
 fs.copyFileSync(
-    path.resolve(__dirname, '../../05-rest/config.docker.js'),
-    path.resolve(__dirname, '../../05-rest/config.js'),
+    path.resolve(__dirname, '../../05-rest/config.docker.ts'),
+    path.resolve(__dirname, '../../05-rest/config.ts'),
 );
