846d44027ae67f4e76b760f3a114875043fe6d20
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-02-12 18:02:38 +0100

    ok

--- 05-rest/config.jlg.ts
+++ 05-rest/config.jlg.ts
@@ -3,5 +3,5 @@ import * as path from 'path';
 export const config = {
     keyFile: path.resolve('/etc/letsencrypt/live/vps.jlg-consulting.com/privkey.pem'),
     crtFile: path.resolve('/etc/letsencrypt/live/vps.jlg-consulting.com/fullchain.pem'),
-    hostHttpsPort: 20443
+    hostHttpsPort: 443
 };
